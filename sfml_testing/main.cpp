973032e5a71a8d64ff70bd12016c72cf655524f0
Yuriy Shevchenko <sevcenkourij244@gmail.com>
2023-04-14 15:08:22 +0300

    v1.0

---
+++ sfml_testing/main.cpp
@@ -0,0 +1,38 @@
+#include <SFML\Graphics.hpp>
+
+#define W_PIX 480
+#define H_PIX 600
+
+static void MainWindowDrawing()
+{ 
+	sf::RenderWindow mainWindow(sf::VideoMode(H_PIX, W_PIX), "Text"); 
+	sf::Event mainWindowEvent;
+	sf::CircleShape circle;;
+
+	circle.setFillColor        (sf::Color::Blue);
+	circle.setOutlineColor     (sf::Color::Green);
+	circle.setRadius           (50);
+	circle.setPosition         (1.f, 1.f);
+	circle.setOutlineThickness (2.f);
+	
+	while (mainWindow.isOpen())
+	{
+		while (mainWindow.pollEvent(mainWindowEvent));   
+		{
+			if (mainWindowEvent.type == sf::Event::Closed) { mainWindow.close(); }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))    { circle.move(0, -0.02); }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))  { circle.move(0, 0.02);  }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))  { circle.move(-0.02, 0); }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { circle.move(0.02, 0);  }
+		}
+		mainWindow.clear   ();
+		mainWindow.draw    (circle);
+		mainWindow.display ();
+	}
+}
+
+int main()
+{
+	MainWindowDrawing();
+	return 0;
+}
